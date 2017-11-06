e83e2ae46940a4592fa2cd9608040d9092e27a88
Jim Yeh <jim@bitmark.com>
2017-11-06 21:14:14 +0800

    Update the bitmarkd version to 6.2
    * Update Dockerfile and its configurations

--- Dockerfile
+++ Dockerfile
@@ -27,22 +27,28 @@ ENV PATH="/go/bin:${PATH}"
 RUN wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash && \
     bash -c "source ~/.nvm/nvm.sh && nvm install v7"
 
-ENV BITMARKD_VERSION 5.6
+ENV BITMARKD_VERSION 6.2
 
 RUN go get -d github.com/bitmark-inc/bitmarkd || \
+    cd /go/src/github.com/bitmark-inc/bitmarkd && git checkout v6.2 && \
     go install -ldflags "-X main.version=$BITMARKD_VERSION" github.com/bitmark-inc/bitmarkd/command/... && \
     go get github.com/bitmark-inc/discovery && \
     go get -d github.com/bitmark-inc/bitmark-wallet && \
     go install github.com/bitmark-inc/bitmark-wallet
 
-RUN go get github.com/bitmark-inc/bitmark-node
+RUN go get github.com/bitmark-inc/exitwithstatus && \
+    go get github.com/gin-gonic/gin && go get github.com/gin-gonic/contrib/static && \
+    go get github.com/coreos/bbolt
+
+COPY . /go/src/github.com/bitmark-inc/bitmark-node
+RUN go install github.com/bitmark-inc/bitmark-node
 RUN cd /go/src/github.com/bitmark-inc/bitmark-node/ui && bash -c "source ~/.nvm/nvm.sh && npm install && npm run build"
 
 ADD bitmark-node.conf.sample /.config/bitmark-node/bitmark-node.conf
 ADD docker-assets/bitmarkd.conf /.config/bitmark-node/bitmarkd/bitmark/
-ADD docker-assets/prooferd.conf /.config/bitmark-node/prooferd/bitmark/
+ADD docker-assets/recorderd.conf /.config/bitmark-node/recorderd/bitmark/
 ADD docker-assets/bitmarkd-test.conf /.config/bitmark-node/bitmarkd/testing/bitmarkd.conf
-ADD docker-assets/prooferd-test.conf /.config/bitmark-node/prooferd/testing/prooferd.conf
+ADD docker-assets/recorderd-test.conf /.config/bitmark-node/recorderd/testing/recorderd.conf
 ADD docker-assets/start.sh /
 
 EXPOSE 2130 2135 2136 2150
--- docker-assets/start.sh
+++ docker-assets/start.sh
@@ -4,17 +4,18 @@
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/bitmark/bitmarkd.conf gen-peer-identity
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/bitmark/bitmarkd.conf gen-rpc-cert
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/bitmark/bitmarkd.conf gen-proof-identity
-prooferd --config-file=/.config/bitmark-node/prooferd/bitmark/prooferd.conf generate-identity
+recorderd --config-file=/.config/bitmark-node/recorderd/bitmark/recorderd.conf generate-identity
 
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/testing/bitmarkd.conf gen-peer-identity
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/testing/bitmarkd.conf gen-rpc-cert
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/testing/bitmarkd.conf gen-proof-identity
-prooferd --config-file=/.config/bitmark-node/prooferd/testing/prooferd.conf generate-identity
+recorderd --config-file=/.config/bitmark-node/recorderd/testing/recorderd.conf generate-identity
 
 # Set the proof public key inot prooferd config
-sed -ie "s/@BITMARKD-PROOFER-PUBLIC-KEY@/$(cat /.config/bitmark-node/bitmarkd/bitmark/proof.public | cut -d":" -f2)/g" /.config/bitmark-node/prooferd/bitmark/prooferd.conf
-sed -ie "s/@BITMARKD-PROOFER-PUBLIC-KEY@/$(cat /.config/bitmark-node/bitmarkd/testing/proof.public | cut -d":" -f2)/g" /.config/bitmark-node/prooferd/testing/prooferd.conf
+sed -ie "s/@BITMARKD-PROOF-PUBLIC-KEY@/$(cat /.config/bitmark-node/bitmarkd/bitmark/proof.public | cut -d":" -f2)/g" /.config/bitmark-node/recorderd/bitmark/recorderd.conf
+sed -ie "s/@BITMARKD-PROOF-PUBLIC-KEY@/$(cat /.config/bitmark-node/bitmarkd/testing/proof.public | cut -d":" -f2)/g" /.config/bitmark-node/recorderd/testing/recorderd.conf
 
 # Start bitmark node
 export CONTAINER_IP=$(awk 'END{print $1}' /etc/hosts)
+cd /.config/bitmark-node
 bitmark-node -config-file=/.config/bitmark-node/bitmark-node.conf -container-ip=$CONTAINER_IP -ui=/go/src/github.com/bitmark-inc/bitmark-node/ui/public
