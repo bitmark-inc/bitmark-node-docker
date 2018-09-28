a9fd3a4c581d9903890e4668ea10c80e01a40177
pieceofr <RachaelPai>
2018-09-28 09:58:23 +0800

    show incoming and outgoing node info

--- server/network.go
+++ server/network.go
@@ -24,10 +24,10 @@ func connCheck(host, port string) bool {
 	}
 }
 
-func getConnectors() int {
+func getPeers() (int, int) {
 	resp, err := client.Get("https://127.0.0.1:2131/bitmarkd/details")
 	if err != nil {
-		return 0
+		return 0, 0
 	}
 	defer resp.Body.Close()
 
@@ -35,25 +35,26 @@ func getConnectors() int {
 	io.Copy(&buf, resp.Body)
 
 	if resp.StatusCode != 200 {
-		return 0
+		return 0, 0
 	}
 
 	var reply DetailReply
 	d := json.NewDecoder(&buf)
 	err = d.Decode(&reply)
 	if err != nil {
-		return 0
+		return 0, 0
 	}
 
-	return int(reply.Peers.Local)
+	return int(reply.Peers.Incoming), int(reply.Peers.Outgoing)
 }
 
 func (ws *WebServer) ConnectionStatus(c *gin.Context) {
 
 	publicIP := os.Getenv("PUBLIC_IP")
-
+	incoming, outgoing := getPeers()
 	c.JSON(200, map[string]interface{}{
-		"connections": getConnectors(),
+		"incoming": incoming,
+		"outgoing": outgoing,
 		"port_state": map[string]interface{}{
 			"listening": connCheck(publicIP, "2136"),
 		},
--- server/structs.go
+++ server/structs.go
@@ -10,12 +10,17 @@ type BlockCounts struct {
 	Remote uint64 `json:"remote"`
 }
 
+type PeerCounts struct {
+	Incoming uint64 `json:"incoming"`
+	Outgoing uint64 `json:"outgoing"`
+}
+
 type DetailReply struct {
 	Chain               string      `json:"chain"`
 	Mode                string      `json:"mode"`
 	Blocks              BlockCounts `json:"blocks"`
 	RPCs                uint64      `json:"rpcs"`
-	Peers               BlockCounts `json:"peers"`
+	Peers               PeerCounts  `json:"peers"`
 	TransactionCounters Counters    `json:"transactionCounters"`
 	Difficulty          float64     `json:"difficulty"`
 	Version             string      `json:"version"`
