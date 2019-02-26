cf8c8ccea27cb1a756b40226951fb1dd3b4fa4e9
Rachael <rachael@bitmark.com>
2019-02-26 17:27:30 +0800

    [Config]Fix livenet proof.sign initialize error

--- docker-assets/start.sh
+++ docker-assets/start.sh
@@ -1,48 +1,36 @@
 #!/bin/sh
 
-# Generate all keys
+
+# Set the data-directory path for bitmarkd and recorder configuration 
+sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/bitmarkd/bitmark:' /.config/bitmark-node/bitmarkd/bitmark/bitmarkd.conf
+sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/recorderd/bitmark:' /.config/bitmark-node/recorderd/bitmark/recorderd.conf
+sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/bitmarkd/testing:' /.config/bitmark-node/bitmarkd/testing/bitmarkd.conf
+sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/recorderd/testing:' /.config/bitmark-node/recorderd/testing/recorderd.conf
+
+# Generate all keys for livenet
+cd /.config/bitmark-node/bitmarkd/bitmark
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/bitmark/bitmarkd.conf gen-peer-identity
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/bitmark/bitmarkd.conf gen-rpc-cert
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/bitmark/bitmarkd.conf gen-proof-identity
 recorderd --config-file=/.config/bitmark-node/recorderd/bitmark/recorderd.conf generate-identity
 rm /.config/bitmark-node/bitmarkd/bitmark/proof.sign
 
-# Copy livenet key to its data directory
-cp peer.private /.config/bitmark-node/bitmarkd/bitmark/peer.private
-cp peer.public /.config/bitmark-node/bitmarkd/bitmark/peer.public
-cp peer.private /.config/bitmark-node/recorderd/bitmark/peer.private
-cp peer.public /.config/bitmark-node/recorderd/bitmark/peer.public
-cp proof.private /.config/bitmark-node/bitmarkd/bitmark/proof.private
-cp proof.public /.config/bitmark-node/bitmarkd/bitmark/proff.public
-cp rpc.crt /.config/bitmark-node/bitmarkd/bitmark/rpc.crt
-cp rpc.key /.config/bitmark-node/bitmarkd/bitmark/rpc.key
 
-
+# Generate all keys for testnet
+cd /.config/bitmark-node/bitmarkd/testing/
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/testing/bitmarkd.conf gen-peer-identity
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/testing/bitmarkd.conf gen-rpc-cert
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/testing/bitmarkd.conf gen-proof-identity
 recorderd --config-file=/.config/bitmark-node/recorderd/testing/recorderd.conf generate-identity
 rm /.config/bitmark-node/bitmarkd/testing/proof.sign
 
-# copy testnet key to its data directory
-cp peer.private /.config/bitmark-node/bitmarkd/testing/peer.private
-cp peer.public /.config/bitmark-node/bitmarkd/testing/peer.public
-cp peer.private /.config/bitmark-node/recorderd/testing/peer.private
-cp peer.public /.config/bitmark-node/recorderd/testing/peer.public
-cp proof.private /.config/bitmark-node/bitmarkd/testing/proof.private
-cp proof.public /.config/bitmark-node/bitmarkd/testing/proof.public
-cp rpc.crt /.config/bitmark-node/bitmarkd/testing/rpc.crt
-cp rpc.key /.config/bitmark-node/bitmarkd/testing/rpc.key
+# move back to root directory
+cd /
 
 # Set the proof public key inot recorderd config
 sed -ie "s/@BITMARKD-PROOF-PUBLIC-KEY@/$(cat /.config/bitmark-node/bitmarkd/bitmark/proof.public | cut -d":" -f2)/g" /.config/bitmark-node/recorderd/bitmark/recorderd.conf
 sed -ie "s/@BITMARKD-PROOF-PUBLIC-KEY@/$(cat /.config/bitmark-node/bitmarkd/testing/proof.public | cut -d":" -f2)/g" /.config/bitmark-node/recorderd/testing/recorderd.conf
 
-# Set the data-directory path for bitmarkd and recorder configuration 
-sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/bitmarkd/bitmark:' /.config/bitmark-node/bitmarkd/bitmark/bitmarkd.conf
-sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/bitmarkd/testing:' /.config/bitmark-node/bitmarkd/testing/bitmarkd.conf
-sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/recorderd/bitmark:' /.config/bitmark-node/recorderd/bitmark/recorderd.conf
-sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/recorderd/testing:' /.config/bitmark-node/recorderd/testing/recorderd.conf
 
 # Start bitmark node
 export CONTAINER_IP=$(awk 'END{print $1}' /etc/hosts)
