256b6362f34c3a1db7c02c17cb0d0a9b63c28af5
Rachael <rachael@bitmark.com>
2019-03-07 14:12:59 +0800

    fix bug of peer key dispear in recorderd

--- docker-assets/start.sh
+++ docker-assets/start.sh
@@ -8,20 +8,34 @@ sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/bitmarkd/testing:' /.config/bi
 sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/recorderd/testing:' /.config/bitmark-node/recorderd/testing/recorderd.conf
 
 # Generate all keys for livenet
+# @ /.config/bitmark-node/bitmarkd/bitmark
 cd /.config/bitmark-node/bitmarkd/bitmark
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/bitmark/bitmarkd.conf gen-peer-identity
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/bitmark/bitmarkd.conf gen-rpc-cert
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/bitmark/bitmarkd.conf gen-proof-identity
+# copy peer keys to recorderd
+cp peer.private /.config/bitmark-node/recorderd/bitmark/peer.private
+cp peer.public /.config/bitmark-node/recorderd/bitmark/peer.public
+# @ /.config/bitmark-node/recorderd/bitmark
+cd /.config/bitmark-node/recorderd/bitmark
 recorderd --config-file=/.config/bitmark-node/recorderd/bitmark/recorderd.conf generate-identity
+# remove proof.sign for livenet
 rm /.config/bitmark-node/bitmarkd/bitmark/proof.sign
 
 
 # Generate all keys for testnet
+# @ /.config/bitmark-node/bitmarkd/testing
 cd /.config/bitmark-node/bitmarkd/testing/
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/testing/bitmarkd.conf gen-peer-identity
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/testing/bitmarkd.conf gen-rpc-cert
 bitmarkd --config-file=/.config/bitmark-node/bitmarkd/testing/bitmarkd.conf gen-proof-identity
+# copy peer keys to recorderd
+cp peer.private /.config/bitmark-node/recorderd/testing/peer.private
+cp peer.public /.config/bitmark-node/recorderd/testing/peer.public
+# @ /.config/bitmark-node/recorderd/testing
+cd /.config/bitmark-node/recorderd/testing
 recorderd --config-file=/.config/bitmark-node/recorderd/testing/recorderd.conf generate-identity
+# remove proof.sign for testnet
 rm /.config/bitmark-node/bitmarkd/testing/proof.sign
 
 # move back to root directory
@@ -31,6 +45,11 @@ cd /
 sed -ie "s/@BITMARKD-PROOF-PUBLIC-KEY@/$(cat /.config/bitmark-node/bitmarkd/bitmark/proof.public | cut -d":" -f2)/g" /.config/bitmark-node/recorderd/bitmark/recorderd.conf
 sed -ie "s/@BITMARKD-PROOF-PUBLIC-KEY@/$(cat /.config/bitmark-node/bitmarkd/testing/proof.public | cut -d":" -f2)/g" /.config/bitmark-node/recorderd/testing/recorderd.conf
 
+# Set the data-directory path for bitmarkd and recorder configuration 
+sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/bitmarkd/bitmark:' /.config/bitmark-node/bitmarkd/bitmark/bitmarkd.conf
+sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/bitmarkd/testing:' /.config/bitmark-node/bitmarkd/testing/bitmarkd.conf
+sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/recorderd/bitmark:' /.config/bitmark-node/recorderd/bitmark/recorderd.conf
+sed -ie 's:@DATA-DIRECTORY@:/.config/bitmark-node/recorderd/testing:' /.config/bitmark-node/recorderd/testing/recorderd.conf
 
 # Start bitmark node
 export CONTAINER_IP=$(awk 'END{print $1}' /etc/hosts)
